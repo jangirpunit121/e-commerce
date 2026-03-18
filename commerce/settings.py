28367805821b0e587d5760aef6572e1505b58a77
Punit Jangir <punitjangir322@gmail.com>
2026-03-18 17:04:21 +0530

    new

--- commerce/settings.py
+++ commerce/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware','corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
