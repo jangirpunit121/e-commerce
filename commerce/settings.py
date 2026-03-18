6e85bdf3a7e89005f025f5d612fc7f1b4d493977
Punit Jangir <punitjangir322@gmail.com>
2026-03-18 17:02:06 +0530

    new

--- commerce/settings.py
+++ commerce/settings.py
@@ -34,7 +34,7 @@ CSRF_TRUSTED_ORIGINS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin','corsheaders',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
