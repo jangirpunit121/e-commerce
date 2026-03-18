778cef692fa3ca796513b0afd6975a8895db4f5c
Punit Jangir <punitjangir322@gmail.com>
2026-03-18 16:54:11 +0530

    new

--- commerce/settings.py
+++ commerce/settings.py
@@ -137,10 +137,4 @@ REST_FRAMEWORK = {
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",  # Agar aap Live Server use kar rahe hain
-    "http://localhost:5500",
-    "http://127.0.0.1:8000",  # Django server
-    "http://localhost:8000",
-    "file://",  # Agar direct file open ki hai
-]
+CORS_ALLOW_ALL_ORIGINS = True
